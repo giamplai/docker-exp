35067ab5f5faa5b29bfa0529bc621bb0980747a5
laig <giampiero.lai@gmail.com>
2018-10-16 23:09:08 +0200

    Update

---
+++ docker-clear.bat
@@ -0,0 +1,6 @@
+@echo off
+docker-compose stop
+docker-compose kill
+FOR /f "tokens=*" %%i IN ('docker ps -aq') DO docker rm %%i
+FOR /f "tokens=*" %%i IN ('docker images --format "{{.ID}}"') DO docker rmi %%i
+docker system prune -af
